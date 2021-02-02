45fdb4872ba0e637aab18b3bd09689c3ed75c56c
Wayne Seltzer <wayne@theseltzers.net>
2021-02-02 09:36:43 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+music.play_melody("C5 B A G F E D C ", 120)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,4 @@
+music.playMelody("C5 B A G F E D C ", 120)
+basic.forever(function on_forever() {
+    
+})
